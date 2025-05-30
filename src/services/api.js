e73302b530f115a0a2db831cc23266e6f00d9128
Ahmed <midots202@gmail.com>
2025-05-30 21:24:33 +0300

    fix: backend utl

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     // baseURL: import.meta.env.VITE_API_BASE_URL,
-    baseURL: "https://react-project-nest-production.up.railway.app/",
+    baseURL: "https://react-project-nest-production.up.railway.app",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
